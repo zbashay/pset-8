dc996924faa12395742273853dd3a98013cea12c
zbashay <ronatorino@gmail.com>
2020-02-09 11:22:22 -0600

    ok

--- js/app.js
+++ js/app.js
@@ -16,6 +16,10 @@ const winningConditions = [
 let board;
 let turn;
 let win;
+let countingxwins = 0
+let countingowins = 0
+let switch_turn_count = 0
+
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
@@ -32,17 +36,50 @@ document.getElementById("reset-button").onclick = init;
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
 function init() {
-  board = ["", "", "", "", "", "", "", "", ""];
+board = ["", "", "", "", "", "", "", "", ""];
+if (switch_turn_count == 0) {
   turn = "X";
-  win = null;
-
-  render();
 }
+else if (switch_turn_count == 1) {
+  turn = "O"
+}
+win = null;
+
+render();
+}
+function switch_turn() {
+if (switch_turn_count == 0) {
+  switch_turn_count = 1
+}
+else if (switch_turn_count == 1) {
+  switch_turn_count = 0
+}
+}
+
+String.prototype.splice = function(idx, rem, str) {
+    return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
+};
 
 function render() {
   board.forEach(function(mark, index) {
     squares[index].textContent = mark;
   });
+
+  if (win === "X") {
+    countingxwins = countingxwins + 1
+  }
+  else if (win === "O") {
+    countingowins = countingowins + 1
+  }
+
+  let change1 = document.getElementById("owins").innerHTML;
+  let change2 = document.getElementById("xwins").innerHTML;
+
+  let new1 = change2.splice(8, 1, countingxwins);
+  let new2 = change1.splice(8, 1, countingowins);
+
+  document.getElementById("xwins").innerHTML = new1;
+  document.getElementById("owins").innerHTML = new2;
 
   message.textContent =
     win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
